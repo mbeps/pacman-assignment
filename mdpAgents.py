bdbf6d4b7e46745be140589b1a153bfe06b3b11f
Maruf Bepary <bepary71@gmail.com>
2024-11-17 11:49:57 +0000

    Initial skeleton for avoiding ghosts - Using Markov Decision Process to make decision - Using Modified Policy Iteration to populate the utilities of the cells to make decisions

--- mdpAgents.py
+++ mdpAgents.py
@@ -1,33 +1,3 @@
-# mdpAgents.py
-# parsons/20-nov-2017
-#
-# Version 1
-#
-# The starting point for CW2.
-#
-# Intended to work with the PacMan AI projects from:
-#
-# http://ai.berkeley.edu/
-#
-# These use a simple API that allow us to control Pacman's interaction with
-# the environment adding a layer on top of the AI Berkeley code.
-#
-# As required by the licensing agreement for the PacMan AI we have:
-#
-# Licensing Information:  You are free to use or extend these projects for
-# educational purposes provided that (1) you do not distribute or publish
-# solutions, (2) you retain this notice, and (3) you provide clear
-# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
-# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
-# The core projects and autograders were primarily created by John DeNero
-# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# Student side autograding was added by Brad Miller, Nick Hay, and
-# Pieter Abbeel (pabbeel@cs.berkeley.edu).
-
-# The agent here is was written by Simon Parsons, based on the code in
-# pacmanAgents.py
-
 from pacman import Directions
 from game import Agent
 import api
@@ -36,28 +6,178 @@ import game
 import util
 
 class MDPAgent(Agent):
-
-    # Constructor: this gets run when we first invoke pacman.py
     def __init__(self):
-        print "Starting up MDPAgent!"
-        name = "Pacman"
-
-    # Gets run after an MDPAgent object is created and once there is
-    # game state to access.
-    def registerInitialState(self, state):
-        print "Running registerInitialState for MDPAgent!"
-        print "I'm at:"
-        print api.whereAmI(state)
+        # MDP Parameters
+        self.discount = 0.9  # Discount factor gamma
+        self.k = 3  # Number of value iterations per policy iteration
+        self.theta = 0.01  # Convergence threshold
         
-    # This is what gets run in between multiple games
-    def final(self, state):
-        print "Looks like the game just ended!"
+        # Reward Parameters
+        self.ghost_penalty = -100  # Penalty for being near a ghost
+        self.ghost_distance_threshold = 3  # Distance to consider ghost threat
+        
+        # Movement probability parameters (from api.py)
+        self.prob_intended = 0.8  # Probability of moving in intended direction
+        self.prob_perpendicular = 0.1  # Probability of moving perpendicular
+        
+        # State tracking
+        self.V = util.Counter()  # Value function
+        self.pi = util.Counter()  # Policy
 
-    # For now I just move randomly
+    def registerInitialState(self, state):
+        # Reset values for new game
+        self.V = util.Counter()
+        self.pi = util.Counter()
+        
     def getAction(self, state):
-        # Get the actions we can try, and remove "STOP" if that is one of them.
+        # Get legal actions
         legal = api.legalActions(state)
         if Directions.STOP in legal:
             legal.remove(Directions.STOP)
-        # Random choice between the legal options.
-        return api.makeMove(random.choice(legal), legal)
+            
+        # Get current state key
+        current_state = self.getStateKey(state)
+        
+        # Run modified policy iteration
+        self.modifiedPolicyIteration(state, legal)
+        
+        # Get best action according to policy
+        best_action = self.pi[current_state]
+        
+        if best_action in legal:
+            return api.makeMove(best_action, legal)
+        else:
+            return api.makeMove(random.choice(legal), legal)
+            
+    def getStateKey(self, state):
+        """Convert state to hashable key - only tracking Pacman and ghost positions"""
+        pacman_pos = api.whereAmI(state)
+        ghost_positions = tuple(sorted(api.ghosts(state)))
+        walls = tuple(sorted(api.walls(state)))
+        return (pacman_pos, ghost_positions, walls)
+
+    def getNextState(self, state_key, action):
+        """Predict next state given current state and action"""
+        pacman_pos, ghost_positions, walls = state_key
+        x, y = pacman_pos
+        
+        # Get next position based on action
+        if action == Directions.NORTH:
+            next_pos = (x, y + 1)
+        elif action == Directions.SOUTH:
+            next_pos = (x, y - 1)
+        elif action == Directions.EAST:
+            next_pos = (x + 1, y)
+        elif action == Directions.WEST:
+            next_pos = (x - 1, y)
+        
+        # Check if next position is a wall
+        if next_pos in walls:
+            next_pos = pacman_pos
+            
+        return (next_pos, ghost_positions, walls)
+            
+    def getReward(self, state_key, action, next_state_key):
+        """Calculate reward for state-action-nextstate transition"""
+        current_pos = state_key[0]
+        ghost_positions = state_key[1]
+        next_pos = next_state_key[0]
+        
+        # If position didn't change (hit wall), penalize
+        if current_pos == next_pos:
+            return -10
+        
+        reward = 0
+        # Check ghost distances from next position
+        for ghost_pos in ghost_positions:
+            dist = util.manhattanDistance(next_pos, ghost_pos)
+            if dist < self.ghost_distance_threshold:
+                reward += self.ghost_penalty / (dist + 1)
+                    
+        return reward
+        
+    def getTransitionProb(self, action):
+        """Get transition probabilities for action"""
+        probs = util.Counter()
+        
+        if action == Directions.NORTH:
+            probs[Directions.NORTH] = self.prob_intended
+            probs[Directions.EAST] = self.prob_perpendicular
+            probs[Directions.WEST] = self.prob_perpendicular
+        elif action == Directions.SOUTH:
+            probs[Directions.SOUTH] = self.prob_intended
+            probs[Directions.EAST] = self.prob_perpendicular
+            probs[Directions.WEST] = self.prob_perpendicular
+        elif action == Directions.EAST:
+            probs[Directions.EAST] = self.prob_intended
+            probs[Directions.NORTH] = self.prob_perpendicular
+            probs[Directions.SOUTH] = self.prob_perpendicular
+        elif action == Directions.WEST:
+            probs[Directions.WEST] = self.prob_intended
+            probs[Directions.NORTH] = self.prob_perpendicular
+            probs[Directions.SOUTH] = self.prob_perpendicular
+            
+        return probs
+        
+    def modifiedPolicyIteration(self, state, legal_actions):
+        """Modified Policy Iteration implementation"""
+        current_state_key = self.getStateKey(state)
+        
+        # Policy evaluation for k steps
+        for _ in range(self.k):
+            delta = 0
+            v = self.V[current_state_key]
+            
+            # If no policy for state yet, initialize randomly
+            if current_state_key not in self.pi:
+                self.pi[current_state_key] = random.choice(legal_actions)
+            
+            action = self.pi[current_state_key]
+            new_v = 0
+            
+            # Get transition probabilities for intended action
+            trans_probs = self.getTransitionProb(action)
+            
+            # Calculate expected value over all possible outcomes
+            for actual_action, prob in trans_probs.items():
+                # Get next state for this actual action
+                next_state_key = self.getNextState(current_state_key, actual_action)
+                # Get reward for this transition
+                reward = self.getReward(current_state_key, actual_action, next_state_key)
+                # Update value using Bellman equation
+                new_v += prob * (reward + self.discount * self.V[next_state_key])
+            
+            self.V[current_state_key] = new_v
+            delta = abs(v - new_v)
+            
+            if delta < self.theta:
+                break
+                
+        # Policy improvement
+        old_action = self.pi[current_state_key]
+        
+        # Find best action
+        max_value = float('-inf')
+        best_action = None
+        
+        # Try all legal actions
+        for action in legal_actions:
+            value = 0
+            trans_probs = self.getTransitionProb(action)
+            
+            # Calculate expected value over all possible outcomes
+            for actual_action, prob in trans_probs.items():
+                next_state_key = self.getNextState(current_state_key, actual_action)
+                reward = self.getReward(current_state_key, actual_action, next_state_key)
+                value += prob * (reward + self.discount * self.V[next_state_key])
+                
+            if value > max_value:
+                max_value = value
+                best_action = action
+                
+        self.pi[current_state_key] = best_action
+        
+    def final(self, state):
+        """Reset state for new game"""
+        self.V = util.Counter()
+        self.pi = util.Counter()
